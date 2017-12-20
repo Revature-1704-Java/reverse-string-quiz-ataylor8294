883edea08fb0d0564f9676da06fbc24ecc14b2f4
Andrew Taylor <andrewmichaeltaylor94@gmail.com>
2017-12-20 10:25:04 -0500

    hello

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -2,6 +2,14 @@ package quiz;
 
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+        //return new StringBuffer(input).reverse().toString();
+        if(input.isEmpty()){
+            return "";
+        }
+        //return new StringBuffer(input).reverse().toString();
+        //uses recursion to reverse
+        else{
+            return input.charAt(input.length()-1) + reverse(input.substring(0, input.length()-1));
+        }
     }
 }
